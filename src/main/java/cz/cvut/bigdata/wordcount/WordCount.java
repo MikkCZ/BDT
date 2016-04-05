2f19368e9295c31b2144577c378c61a54e8c4f75
Michal Stanke <michal@stanke.cz>
2016-04-05 13:33:57 +0200

    Format code

--- src/main/java/cz/cvut/bigdata/wordcount/WordCount.java
+++ src/main/java/cz/cvut/bigdata/wordcount/WordCount.java
@@ -29,175 +29,164 @@ import cz.cvut.bigdata.cli.ArgumentParser;
  * This is a very simple extension of basic WordCount Example implemented using
  * a new MapReduce API.
  */
-public class WordCount extends Configured implements Tool
-{
-    /**
-     * The main entry of the application.
-     */
-    public static void main(String[] arguments) throws Exception
-    {
-        System.exit(ToolRunner.run(new WordCount(), arguments));
-    }
+public class WordCount extends Configured implements Tool {
+	/**
+	 * The main entry of the application.
+	 */
+	public static void main(String[] arguments) throws Exception {
+		System.exit(ToolRunner.run(new WordCount(), arguments));
+	}
 
-    /**
-     * Sorts word keys in reversed lexicographical order.
-     */
-    public static class WordCountComparator extends WritableComparator
-    {
-        protected WordCountComparator()
-        {
-            super(Text.class, true);
-        }
-        
-        @SuppressWarnings({ "rawtypes", "unchecked" })
-		@Override public int compare(WritableComparable a, WritableComparable b)
-        {
-            // Here we use exploit the implementation of compareTo(...) in Text.class.
-            return -a.compareTo(b);
-        }
-    }
+	/**
+	 * Sorts word keys in reversed lexicographical order.
+	 */
+	public static class WordCountComparator extends WritableComparator {
+		protected WordCountComparator() {
+			super(Text.class, true);
+		}
 
-    /**
-     * (word, count) pairs are sent to reducers based on the length of the word.
-     * 
-     * The maximum length of a word that will be processed by
-     * the target reducer. For example, using 3 reducers the
-     * word length span processed by the reducers would be:
-     * reducer     lengths processed
-     * -------     -----------------
-     *  00000           1 -- 14
-     *  00001          15 -- 29
-     *  00003          30 -- OO
-     */
-    public static class WordLengthPartitioner extends Partitioner<Text, IntWritable>
-    {
-        private static final int MAXIMUM_LENGTH_SPAN = 30;
-        
-        @Override public int getPartition(Text key, IntWritable value, int numOfPartitions)
-        {
-            if (numOfPartitions == 1)
-                return 0;
-            
-            int lengthSpan = Math.max(MAXIMUM_LENGTH_SPAN / (numOfPartitions - 1), 1);
-            
-            return Math.min(Math.max(0, (key.toString().length() - 1) / lengthSpan), numOfPartitions - 1);
-        }
-    }
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		@Override
+		public int compare(WritableComparable a, WritableComparable b) {
+			// Here we use exploit the implementation of compareTo(...) in
+			// Text.class.
+			return -a.compareTo(b);
+		}
+	}
 
-    /**
-     * Receives (byteOffsetOfLine, textOfLine), note we do not care about the type of the key
-     * because we do not use it anyway, and emits (word, 1) for each occurrence of the word
-     * in the line of text (i.e. the received value).
-     */
-    public static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable>
-    {
-        private final IntWritable ONE = new IntWritable(1);
-        private Text word = new Text();
+	/**
+	 * (word, count) pairs are sent to reducers based on the length of the word.
+	 * 
+	 * The maximum length of a word that will be processed by the target
+	 * reducer. For example, using 3 reducers the word length span processed by
+	 * the reducers would be: reducer lengths processed -------
+	 * ----------------- 00000 1 -- 14 00001 15 -- 29 00003 30 -- OO
+	 */
+	public static class WordLengthPartitioner extends Partitioner<Text, IntWritable> {
+		private static final int MAXIMUM_LENGTH_SPAN = 30;
 
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException
-        {
-            String[] words = value.toString().split(" ");
+		@Override
+		public int getPartition(Text key, IntWritable value, int numOfPartitions) {
+			if (numOfPartitions == 1)
+				return 0;
 
-            for (String term : words)
-            {
-                word.set(term);
-                context.write(word, ONE);
-            }
-        }
-    }
+			int lengthSpan = Math.max(MAXIMUM_LENGTH_SPAN / (numOfPartitions - 1), 1);
 
-    /**
-     * Receives (word, list[1, 1, 1, 1, ..., 1]) where the number of 1 corresponds to the total number
-     * of times the word occurred in the input text, which is precisely the value the reducer emits along
-     * with the original word as the key. 
-     * 
-     * NOTE: The received list may not contain only 1s if a combiner is used.
-     */
-    public static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>
-    {
-        public void reduce(Text text, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
-        {
-            int sum = 0;
+			return Math.min(Math.max(0, (key.toString().length() - 1) / lengthSpan), numOfPartitions - 1);
+		}
+	}
 
-            for (IntWritable value : values)
-            {
-                sum += value.get();
-            }
+	/**
+	 * Receives (byteOffsetOfLine, textOfLine), note we do not care about the
+	 * type of the key because we do not use it anyway, and emits (word, 1) for
+	 * each occurrence of the word in the line of text (i.e. the received
+	 * value).
+	 */
+	public static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+		private final IntWritable ONE = new IntWritable(1);
+		private Text word = new Text();
 
-            context.write(text, new IntWritable(sum));
-        }
-    }
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			String[] words = value.toString().split(" ");
 
-    /**
-     * This is where the MapReduce job is configured and being launched.
-     */
-    @Override public int run(String[] arguments) throws Exception
-    {
-        ArgumentParser parser = new ArgumentParser("WordCount");
+			for (String term : words) {
+				word.set(term);
+				context.write(word, ONE);
+			}
+		}
+	}
 
-        parser.addArgument("input", true, true, "specify input directory");
-        parser.addArgument("output", true, true, "specify output directory");
+	/**
+	 * Receives (word, list[1, 1, 1, 1, ..., 1]) where the number of 1
+	 * corresponds to the total number of times the word occurred in the input
+	 * text, which is precisely the value the reducer emits along with the
+	 * original word as the key.
+	 * 
+	 * NOTE: The received list may not contain only 1s if a combiner is used.
+	 */
+	public static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text text, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
 
-        parser.parseAndCheck(arguments);
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
 
-        Path inputPath = new Path(parser.getString("input"));
-        Path outputDir = new Path(parser.getString("output"));
+			context.write(text, new IntWritable(sum));
+		}
+	}
 
-        // Create configuration.
-        Configuration conf = getConf();
-        
-        // Using the following line instead of the previous 
-        // would result in using the default configuration
-        // settings. You would not have a change, for example,
-        // to set the number of reduce tasks (to 5 in this
-        // example) by specifying: -D mapred.reduce.tasks=5
-        // when running the job from the console.
-        //
-        // Configuration conf = new Configuration(true);
+	/**
+	 * This is where the MapReduce job is configured and being launched.
+	 */
+	@Override
+	public int run(String[] arguments) throws Exception {
+		ArgumentParser parser = new ArgumentParser("WordCount");
 
-        // Create job.
-        Job job = Job.getInstance(conf, "WordCount");
-        job.setJarByClass(WordCountMapper.class);
+		parser.addArgument("input", true, true, "specify input directory");
+		parser.addArgument("output", true, true, "specify output directory");
 
-        // Setup MapReduce.
-        job.setMapperClass(WordCountMapper.class);
-        job.setReducerClass(WordCountReducer.class);
+		parser.parseAndCheck(arguments);
 
-        // Make use of a combiner - in this simple case
-        // it is the same as the reducer.
-        job.setCombinerClass(WordCountReducer.class);
+		Path inputPath = new Path(parser.getString("input"));
+		Path outputDir = new Path(parser.getString("output"));
 
-        // Sort the output words in reversed order.
-        job.setSortComparatorClass(WordCountComparator.class);
-        
-        // Use custom partitioner.
-        job.setPartitionerClass(WordLengthPartitioner.class);
+		// Create configuration.
+		Configuration conf = getConf();
 
-        // By default, the number of reducers is configured
-        // to be 1, similarly you can set up the number of
-        // reducers with the following line.
-        //
-        // job.setNumReduceTasks(1);
+		// Using the following line instead of the previous
+		// would result in using the default configuration
+		// settings. You would not have a change, for example,
+		// to set the number of reduce tasks (to 5 in this
+		// example) by specifying: -D mapred.reduce.tasks=5
+		// when running the job from the console.
+		//
+		// Configuration conf = new Configuration(true);
 
-        // Specify (key, value).
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+		// Create job.
+		Job job = Job.getInstance(conf, "WordCount");
+		job.setJarByClass(WordCountMapper.class);
 
-        // Input.
-        FileInputFormat.addInputPath(job, inputPath);
-        job.setInputFormatClass(TextInputFormat.class);
+		// Setup MapReduce.
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
 
-        // Output.
-        FileOutputFormat.setOutputPath(job, outputDir);
-        job.setOutputFormatClass(TextOutputFormat.class);
+		// Make use of a combiner - in this simple case
+		// it is the same as the reducer.
+		job.setCombinerClass(WordCountReducer.class);
 
-        FileSystem hdfs = FileSystem.get(conf);
+		// Sort the output words in reversed order.
+		job.setSortComparatorClass(WordCountComparator.class);
 
-        // Delete output directory (if exists).
-        if (hdfs.exists(outputDir))
-            hdfs.delete(outputDir, true);
+		// Use custom partitioner.
+		job.setPartitionerClass(WordLengthPartitioner.class);
 
-        // Execute the job.
-        return job.waitForCompletion(true) ? 0 : 1;
-    }
+		// By default, the number of reducers is configured
+		// to be 1, similarly you can set up the number of
+		// reducers with the following line.
+		//
+		// job.setNumReduceTasks(1);
+
+		// Specify (key, value).
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		// Input.
+		FileInputFormat.addInputPath(job, inputPath);
+		job.setInputFormatClass(TextInputFormat.class);
+
+		// Output.
+		FileOutputFormat.setOutputPath(job, outputDir);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileSystem hdfs = FileSystem.get(conf);
+
+		// Delete output directory (if exists).
+		if (hdfs.exists(outputDir))
+			hdfs.delete(outputDir, true);
+
+		// Execute the job.
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
 }
