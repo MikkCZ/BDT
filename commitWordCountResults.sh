97cc0ff0264905be0248b9bf09a0dad08c921e6c
Michal Stanke <michal@stanke.cz>
2016-04-05 14:52:37 +0200

    Better results committing

--- commitWordCountResults.sh
+++ commitWordCountResults.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cp /hdfs/user/stankmic/STANKMIC_task1/part-r-00000 /hdfs/bigdata/STANKMIC_task1.txt
+hadoop fs -rm /bigdata/STANKMIC_task1.txt
+hadoop fs -mv /user/stankmic/STANKMIC_task1/part-r-00000 /bigdata/STANKMIC_task1.txt
+hadoop fs -rm -r /user/stankmic/STANKMIC_task1
 
