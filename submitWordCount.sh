f421c459b0c8f3f4d402686ddd29d361f45365a1
Michal Stanke <michal@stanke.cz>
2016-04-05 14:28:25 +0200

    Add org.apache.commons.commons-lang3 and build uber jar

--- submitWordCount.sh
+++ submitWordCount.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-hadoop jar WordCountExtended.jar --input /bigdata/enwiki-latest-pages-articles.txt --output STANKMIC_task1
+hadoop jar WordCountExtended-jar-with-dependencies.jar --input /bigdata/enwiki-latest-pages-articles.txt --output STANKMIC_task1
 
--- to-hador.sh
+++ to-hador.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean package
-scp target/WordCountExtended.jar zuphux.metacentrum.cz:/storage/brno2/home/stankmic/
+scp target/WordCountExtended-jar-with-dependencies.jar zuphux.metacentrum.cz:/storage/brno2/home/stankmic/
 
